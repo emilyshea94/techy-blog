51e9e2193ff8e1551e84ee274cb0fcebef5ea252
emily shea <emilyshea947@gmail.com>
2024-03-28 20:54:04 -0400

    successfully troubleshooted server/ i passport

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ passport.deserializeUser((id, done) => {
 });
 
 // Server setup
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
